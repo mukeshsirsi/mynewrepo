e3bb2b359f23ef606c3695c02ee68583cf29e4e8
mukeshsirsi <108171264+mukeshsirsi@users.noreply.github.com>
2023-03-08 18:53:02 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 install rootterminal
 sudo yum update -y
 sudo amazon-linux-extras install docker
-/home/ec2-user
+/home/ec2-user/mukesh
 sudo service docker start
 sudo usermod -a -G docker ec2-user
