04281bc67d31757bc219a5e87c83b67a87eb4aec
mukeshsirsi <108171264+mukeshsirsi@users.noreply.github.com>
2023-03-08 20:23:03 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@
 sudo apt-get update
 install rootterminal
 sudo yum update -y
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
 sudo amazon-linux-extras install docker
 /home/ec2-user/mukesh
 sudo service docker start
